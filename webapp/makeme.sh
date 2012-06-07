9874a941baca0f56f1b9fce6ad4dc9c5afd79401
Tom Stovall <stovak@gmail.com>
2012-06-07 11:39:07 -0700

    updating makeme.sh

--- webapp/makeme.sh
+++ webapp/makeme.sh
@@ -3,3 +3,5 @@
 #works with drush 5.1 or newer installed
 
 drush make --prepare-install --working-copy --no-gitinfofile --no-cache -y --force-complete apigee.make
+git clone git@github.com:apigee/drupal-settings.git ../drupal-settings
+cp -R ../drupal-settings/sites/* sites
