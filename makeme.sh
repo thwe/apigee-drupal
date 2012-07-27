f761c9e912c2b1fe74a30a100305186b4b10cf9f
Tom Stovall <stovak@gmail.com>
2012-07-27 14:50:19 -0700

    Moving apigee.make and make me to root folder

--- makeme.sh
+++ makeme.sh
@@ -2,6 +2,6 @@
 
 #works with drush 5.1 or newer installed
 
-drush make --prepare-install --working-copy --no-gitinfofile --no-cache -y --force-complete apigee.make
+drush make --prepare-install --working-copy --no-gitinfofile --no-cache -y --force-complete apigee.make webapp
 git clone git@github.com:apigee/drupal-settings.git ../drupal-settings
 cp -R ../drupal-settings/sites/* sites
